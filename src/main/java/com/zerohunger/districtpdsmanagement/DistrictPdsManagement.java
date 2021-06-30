61476e57cd38c219983337290d048bd01d633ece
Gaurav Swain <gswain3316@live.com>
2021-06-30 19:03:43 +0530

    Create maven.yml

---
+++ src/main/java/com/zerohunger/districtpdsmanagement/DistrictPdsManagement.java
@@ -0,0 +1,13 @@
+package com.zerohunger.districtpdsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistrictPdsManagement {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistrictPdsManagement.class, args);
+	}
+	
+}
---
+++ src/main/java/com/zerohunger/districtpdsmanagement/controller/DistrictManagementController.java
@@ -0,0 +1,10 @@
+package com.zerohunger.districtpdsmanagement.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pdsapp/district")
+public class DistrictManagementController {
+
+}
