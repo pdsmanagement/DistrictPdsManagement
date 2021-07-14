f3b1546abf111458ced40d177895bd5c7dda576f
gswain3316 <gswain3316@live.com>
2021-07-15 00:40:26 +0530

    isStateIndicator attribute added to all domain classes

--- src/main/java/com/zerohunger/districtpdsmanagement/repository/DistrictRepository.java
+++ src/main/java/com/zerohunger/districtpdsmanagement/repository/DistrictRepository.java
@@ -3,11 +3,15 @@ package com.zerohunger.districtpdsmanagement.repository;
 import com.zerohunger.districtpdsmanagement.domain.GovBody;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DistrictRepository extends MongoRepository<GovBody, String> {
 
-	GovBody findOneByDistrictName(final String stateName);
+	GovBody findOneByDistrictName(final String districtName);
+
+	@Query("{'stateName' : ?0, 'isStateIndicator' : ?1}")
+    GovBody findGovBodyByStateNameAndIsStateIndicator(final String districtName, final Boolean isStateIndicator);
 	
 }
